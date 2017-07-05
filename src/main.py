7cb74ef3aabe03c093a349df8af7cbdc0988eb0b
Franco Frizzo <francofrizzo@gmail.com>
2017-07-04 23:27:36 -0300

    Change main.py user interface to behave as expected

--- src/main.py
+++ src/main.py
@@ -1,16 +1,50 @@
+#! /usr/bin/env python
 #! coding: utf-8
 
 from __future__ import print_function
 from lambdacalculus import parser, LambdaError
-import readline, sys
+import sys, readline, optparse
 
-while True:
+
+usage = "usage: %prog [options] [expression]"
+
+opt_parser = optparse.OptionParser(usage=usage)
+opt_parser.add_option("-i", "--interactive", dest="interactive",
+                      action="store_true", default=False,
+                      help="use interactive prompt")
+
+options, args = opt_parser.parse_args()
+
+
+def input_exp(prompt=""):
     try:
-        exp_str = raw_input('λ> ')
+        exp_str = raw_input(prompt)
     except EOFError:
-        break
+        return False
+    return exp_str if exp_str != ":q" else False
+
+
+def parse_and_print(exp_str):
     try:
         value = parser.apply_parser(exp_str)
         print(value.str_with_type())
+        return True
     except LambdaError as e:
         print(e, file=sys.stderr)
+        return False
+
+
+if options.interactive:
+    while True:
+        exp_str = input_exp(prompt="λ> ")
+        if exp_str:
+            parse_and_print(exp_str)
+        else:
+            break
+else:
+    exp_str = sys.argv[1] if len(sys.argv) > 1 else input_exp()
+    status = parse_and_print(exp_str)
+    if status:
+        exit(0)
+    else:
+        exit(1)
